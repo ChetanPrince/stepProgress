01caf203896afa4dc15da7cb0b449e086140d401
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-13 11:47:56 +0200

    added basic funcitonality

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const container = document.getElementById("conatainer");
+
+function progressAction(){
+    const container = document.getElementById("container");
+    const style = document.createElement("style");
+    document.head.appendChild(style);
+    const sheet = style.sheet;
+
+    if ("insertRule" in sheet) {
+        sheet.insertRule("#container:after { animation: anim 5s linear forwards; content: ' '; }", sheet.cssRules.length);
+    } else if ("addRule" in sheet) {
+        sheet.addRule("#container:after", "animation: anim 5s linear forwards; content: ' ';", sheet.cssRules.length);
+    }
+}
+
+
 const btnProgress = document.getElementById("next-button");
 
-btnProgress.addEventListener("click")
+btnProgress.addEventListener("click", progressAction)
