74f9055f402c798b733ff669b0808fe8f2f286ad
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-14 11:55:35 +0200

    one step ahead

--- script2.js
+++ script2.js
@@ -1,1 +1,11 @@
-console.log("step Bar Progression");
+const proContainers =document.querySelectorAll(".progress-container");
+function progressAction(){
+    proContainers.forEach(proContainer => {
+        proContainer.classList.add("show");
+    });
+}
+
+
+const btnProgress = document.getElementById("next-button");
+
+btnProgress.addEventListener("click", progressAction)
