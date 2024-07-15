091e53b83dfff0f16fafaed5972f849d1b4d42b4
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-15 02:39:24 +0200

    starting with prev and next functions

--- script2.js
+++ script2.js
@@ -1,12 +1,24 @@
 const proContainers =document.querySelectorAll(".progress-container");
 function progressAction(){
     proContainers.forEach(proContainer => {
-        proContainer.classList.add("show");
+        
+
+            proContainer.classList.add("show");
+
+    });
+}
+function progressActionReverse(){
+    proContainers.forEach(proContainer => {
+        
+            proContainer.classList.remove("show");
+      
+        
     });
 }
 
 
-const btnProgress = document.getElementById("next-button");
+const nextBtn = document.getElementById("next");
+nextBtn.addEventListener("click", progressAction);
 
-
-btnProgress.addEventListener("click", progressAction)
+const prevBtn = document.getElementById("previous");
+prevBtn.addEventListener("click", progressActionReverse);
